d8ca28abcf18cde3f1f58890e1c709c7e4add77d
Vicky Ledsom <victorialedsom@gmail.com>
2022-01-04 12:12:49 +0000

    Fix broken widgets

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.clock import Clock
 from kivymd.uix.screen import Screen
 
 from weather import Weather
+from weather import generate_widgets
 from currentTime import Time
 
 Config.set("graphics", "height", "480")
@@ -28,121 +29,31 @@ class WeatherApp(App):
       # Update the weather every 30mins
       Clock.schedule_interval(weather.update_weather, 1800)
 
-      # ///////////////////////////////
-      # TEMPORARY, move these somewhere better!
-      currentTemperature = Label(
-        text = f'{str(weather.data[0].temp)}°', 
-        pos_hint={'center_x':0.17,'center_y':0.22},
-        font_size= 22,
-        bold= True,
-      )
-      currentIcon = Image(
-        source=f'./icons/{weather.data[0].icon}.png', 
-        pos_hint={'center_x':0.17,'center_y':0.385},
-        size_hint_x= 0.24,
-        allow_stretch= True
-      )
-
-      # in 1hr
-      secondHour = Label(
-        text = convert_hour(weather.data[1].hour), 
-        pos_hint={'center_x':0.4,'center_y':0.41},
-        font_size= 15,
-      )
-      secondTemperature = Label(
-        text = f'{str(weather.data[1].temp)}°', 
-        pos_hint={'center_x':0.4,'center_y':0.22},
-        font_size= 19,
-        bold= True,
-      )
-      secondIcon = Image(
-        source=f'./icons/{weather.data[1].icon}.png', 
-        pos_hint={'center_x':0.4,'center_y':0.31},
-        size_hint_x= 0.09,
-        allow_stretch= True
-      )
-
-      # in 2hrs
-      thirdHour = Label(
-        text = convert_hour(weather.data[2].hour), 
-        pos_hint={'center_x':0.55,'center_y':0.41},
-        font_size= 15,
-      )
-      thirdTemperature = Label(
-        text = f'{str(weather.data[2].temp)}°', 
-        pos_hint={'center_x':0.55,'center_y':0.22},
-        font_size= 19,
-        bold= True,
-      )
-      thirdIcon = Image(
-        source=f'./icons/{weather.data[2].icon}.png', 
-        pos_hint={'center_x':0.55,'center_y':0.31},
-        size_hint_x= 0.09,
-        allow_stretch= True
-      )
-
-      # in 3hrs
-      fourthHour = Label(
-        text = convert_hour(weather.data[3].hour), 
-        pos_hint={'center_x':0.7,'center_y':0.41},
-        font_size= 15,
-      )
-      fourthTemperature = Label(
-        text = f'{str(weather.data[3].temp)}°', 
-        pos_hint={'center_x':0.7,'center_y':0.22},
-        font_size= 19,
-        bold= True,
-      )
-      fourthIcon = Image(
-        source=f'./icons/{weather.data[3].icon}.png', 
-        pos_hint={'center_x':0.7,'center_y':0.31},
-        size_hint_x= 0.09,
-        allow_stretch= True
-      )
-
-      # in 4hrs
-      fifthHour = Label(
-        text = convert_hour(weather.data[4].hour), 
-        pos_hint={'center_x':0.85,'center_y':0.41},
-        font_size= 15,
-      )
-      fifthTemperature = Label(
-        text = f'{str(weather.data[4].temp)}°', 
-        pos_hint={'center_x':0.85,'center_y':0.22},
-        font_size= 19,
-        bold= True,
-      )
-      fifthIcon = Image(
-        source=f'./icons/{weather.data[4].icon}.png', 
-        pos_hint={'center_x':0.85,'center_y':0.31},
-        size_hint_x= 0.09,
-        allow_stretch= True
-      )
-      # ///////////////////////////////
+      weatherWidgets = generate_widgets(weather.data)
 
       # Add temp and icon for current weather
-      screen.add_widget(currentTemperature)
-      screen.add_widget(currentIcon)
+      screen.add_widget(weatherWidgets['currentTemperature'])
+      screen.add_widget(weatherWidgets['currentIcon'])
 
       # Add temp and icon for weather in an hour
-      screen.add_widget(secondTemperature)
-      screen.add_widget(secondIcon)
-      screen.add_widget(secondHour)
+      screen.add_widget(weatherWidgets['secondTemperature'])
+      screen.add_widget(weatherWidgets['secondIcon'])
+      screen.add_widget(weatherWidgets['secondHour'])
 
       # Add temp and icon for weather in 2 hours
-      screen.add_widget(thirdTemperature)
-      screen.add_widget(thirdIcon)
-      screen.add_widget(thirdHour)
+      screen.add_widget(weatherWidgets['thirdTemperature'])
+      screen.add_widget(weatherWidgets['thirdIcon'])
+      screen.add_widget(weatherWidgets['thirdHour'])
 
       # Add temp and icon for weather in 3 hours
-      screen.add_widget(fourthTemperature)
-      screen.add_widget(fourthIcon)
-      screen.add_widget(fourthHour)
+      screen.add_widget(weatherWidgets['fourthTemperature'])
+      screen.add_widget(weatherWidgets['fourthIcon'])
+      screen.add_widget(weatherWidgets['fourthHour'])
 
       # Add temp and icon for weather in 4 hours
-      screen.add_widget(fifthTemperature)
-      screen.add_widget(fifthIcon)
-      screen.add_widget(fifthHour)
+      screen.add_widget(weatherWidgets['fifthTemperature'])
+      screen.add_widget(weatherWidgets['fifthIcon'])
+      screen.add_widget(weatherWidgets['fifthHour'])
 
       return screen
 
--- weather.py
+++ weather.py
@@ -38,6 +38,113 @@ def convert_hour(hour):
 
   return f'{hour} am'
 
+def generate_widgets(data):
+  currentTemperature = Label(
+    text = f'{str(data[0].temp)}°', 
+    pos_hint={'center_x':0.17,'center_y':0.22},
+    font_size= 22,
+    bold= True,
+  )
+  currentIcon = Image(
+    source=f'./icons/{data[0].icon}.png', 
+    pos_hint={'center_x':0.17,'center_y':0.385},
+    size_hint_x= 0.24,
+    allow_stretch= True,
+  )
+
+  # in 1hr
+  secondHour = Label(
+    text = convert_hour(data[1].hour), 
+    pos_hint={'center_x':0.4,'center_y':0.41},
+    font_size= 15,
+  )
+  secondTemperature = Label(
+    text = f'{str(data[1].temp)}°', 
+    pos_hint={'center_x':0.4,'center_y':0.22},
+    font_size= 19,
+    bold= True,
+  )
+  secondIcon = Image(
+    source=f'./icons/{data[1].icon}.png', 
+    pos_hint={'center_x':0.4,'center_y':0.31},
+    size_hint_x= 0.09,
+    allow_stretch= True,
+  )
+
+  # in 2hrs
+  thirdHour = Label(
+    text = convert_hour(data[2].hour), 
+    pos_hint={'center_x':0.55,'center_y':0.41},
+    font_size= 15,
+  )
+  thirdTemperature = Label(
+    text = f'{str(data[2].temp)}°', 
+    pos_hint={'center_x':0.55,'center_y':0.22},
+    font_size= 19,
+    bold= True,
+  )
+  thirdIcon = Image(
+    source=f'./icons/{data[2].icon}.png', 
+    pos_hint={'center_x':0.55,'center_y':0.31},
+    size_hint_x= 0.09,
+    allow_stretch= True,
+  )
+
+  # in 3hrs
+  fourthHour = Label(
+    text = convert_hour(data[3].hour), 
+    pos_hint={'center_x':0.7,'center_y':0.41},
+    font_size= 15,
+  )
+  fourthTemperature = Label(
+    text = f'{str(data[3].temp)}°', 
+    pos_hint={'center_x':0.7,'center_y':0.22},
+    font_size= 19,
+    bold= True,
+  )
+  fourthIcon = Image(
+    source=f'./icons/{data[3].icon}.png', 
+    pos_hint={'center_x':0.7,'center_y':0.31},
+    size_hint_x= 0.09,
+    allow_stretch= True,
+  )
+
+  # in 4hrs
+  fifthHour = Label(
+    text = convert_hour(data[4].hour), 
+    pos_hint={'center_x':0.85,'center_y':0.41},
+    font_size= 15,
+  )
+  fifthTemperature = Label(
+    text = f'{str(data[4].temp)}°', 
+    pos_hint={'center_x':0.85,'center_y':0.22},
+    font_size= 19,
+    bold= True,
+  )
+  fifthIcon = Image(
+    source=f'./icons/{data[4].icon}.png', 
+    pos_hint={'center_x':0.85,'center_y':0.31},
+    size_hint_x= 0.09,
+    allow_stretch= True,
+  )
+
+  return { 
+    'currentTemperature': currentTemperature,
+    'currentIcon': currentIcon,
+    'secondTemperature': secondTemperature,
+    'secondIcon': secondIcon,
+    'secondHour': secondHour,
+    'thirdTemperature': thirdTemperature,
+    'thirdIcon': thirdIcon,
+    'thirdHour': thirdHour,
+    'fourthTemperature': fourthTemperature,
+    'fourthIcon': fourthIcon,
+    'fourthHour': fourthHour,
+    'fifthTemperature': fifthTemperature,
+    'fifthIcon': fifthIcon,
+    'fifthHour': fifthHour,
+}
+
 # data example:
 # {'feelsLike': 4, 'hour': 22, 'icon': '10n', 'temp': 5}
 class Weather(Label):
